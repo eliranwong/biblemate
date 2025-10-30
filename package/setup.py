63bb220094073e04978fe948d789d07e3263ffe7
eliranwong <hkwebtech@gmail.com>
2025-10-30 20:53:02 +0000

    fixed mcp backend option

--- package/setup.py
+++ package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools.command.install import install
 import os, shutil, platform, sys
 
-version = "0.2.21"
+version = "0.2.28"
 with open(os.path.join("biblemate", "version.txt"), "w", encoding="utf-8") as fileObj:
     fileObj.write(version)
 
@@ -69,7 +69,7 @@ setup(
         "console_scripts": [
             f"bm={package}.main:main",
             f"{package}={package}.main:main",
-            f"{package}mcp={package}.main:mcp",
+            f"{package}mcp={package}.biblematemcp:mcp",
         ],
     },
     keywords="mcp agent toolmate ai anthropic azure chatgpt cohere deepseek genai github googleai groq llamacpp mistral ollama openai vertexai xai",
